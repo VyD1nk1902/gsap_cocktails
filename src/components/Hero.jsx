f60373188440575f026c8886c711442c852fc875
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-11-20 11:40:18 +0700

    Implement Cocktails section with animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ const Hero = () => {
         .to(".left-leaf", {y: -200}, 0);
 
     const startValue = isMobile ? "top 50%" : "center 60%";
-    const endValue = isMobile ? "150% top" : "120% top";
+    const endValue = isMobile ? "140% top" : "bottom top";
 
     const tl = gsap.timeline({
       scrollTrigger: {
