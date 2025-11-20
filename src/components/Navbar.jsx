865463654821cfb2af29b6068c13cf135a3e17e9
VyD1nk1902 <vy.dinh.2929@gmail.com>
2025-11-20 17:10:25 +0700

    Fix syntax inside navTween.fromTo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     navTween.fromTo("nav", {backgroundColor: "transparent"},
         {
           backgroundColor: "#00000050",
-          backgroundFilter: "blue(10px)",
+          backdropFilter: "blue(10px)",
           duration: 1,
           ease: "power1.out",
         });
